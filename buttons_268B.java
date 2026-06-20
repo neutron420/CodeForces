7b4c98a51b374cecbbf88d79a1225c7b291bec8f
neutron420 <rihankumar2004@gmail.com>
2026-06-20 20:57:42 +0530

    feat: add solution for Codeforces problem 268B (Buttons)

---
+++ buttons_268B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class buttons_268B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long ans = n;
+        for (long i = 1; i < n; i++) {
+            ans += (n - i) * i;
+        }
+        System.out.println(ans);
+        sc.close();
+    }
+}
